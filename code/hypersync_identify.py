98b1c35785f6bebf70bb89a18bc31e43a1f55e64
Maxime Lucas <ml.maximelucas@gmail.com>
2023-09-17 18:14:54 +0200

    feat: improved kcluster identification and cluster sizes

--- code/hypersync_identify.py
+++ code/hypersync_identify.py
@@ -38,8 +38,8 @@ def identify_state(thetas, t=-1, atol=1e-3):
     """
 
     R1 = order_parameter(thetas, order=1)
-    R2 = order_parameter(thetas, order=2)
-    R3 = order_parameter(thetas, order=3)
+    # R2 = order_parameter(thetas, order=2)
+    # R3 = order_parameter(thetas, order=3)
     diff = np.diff(thetas[:, t], append=thetas[0, t]) % (2 * np.pi)
     is_diff_zero = np.isclose(diff, 0, atol=atol) + np.isclose(
         diff, 2 * np.pi, atol=atol
@@ -49,18 +49,83 @@ def identify_state(thetas, t=-1, atol=1e-3):
     sorted_thetas = np.sort(thetas, axis=0)  # sort along node axis
     q_sorted, is_splay = identify_winding_number(sorted_thetas, t=-1)
 
+    is_2clust, sizes2 = identify_k_clusters(thetas, k=2, t=-1, atol=1e-2)
+    is_3clust, sizes3 = identify_k_clusters(thetas, k=3, t=-1, atol=1e-2)
+
     if is_twisted:
         return f"{q}-twisted"
     elif is_splay and q_sorted == 1:
         return "splay"
     elif np.isclose(R1[t], 1, atol=atol) and np.all(is_diff_zero):
         return "sync"
-    elif np.isclose(R2[t], 1, atol=atol):
+    elif is_2clust:
         return "2-cluster"
-    elif np.isclose(R3[t], 1, atol=atol):
+    elif is_3clust:
         return "3-cluster"
     else:
         return "other"
+
+
+def identify_k_clusters(thetas, k, t, atol=1e-2):
+    """
+    Check if k-cluster state.
+
+    A k-cluster state has k evenly spaced clusters on the circle.
+
+    Parameters:
+    -----------
+    thetas : array-like, shape (n_phases, n_time_steps)
+        Phases of the oscillators over time
+    k : int
+        Number of clusters
+    t : int
+        The index of the time step for which the winding number and twisted state are calculated.
+
+    Returns:
+    --------
+    is_k_clusters : bool
+        True if the state is a k-cluster. False otherwise.
+    sizes : tuple of float
+        The relative sizes of each cluster.
+    """
+
+    n_clust = k
+    dist = 2 * np.pi / n_clust
+    N = len(thetas)
+
+    psi = thetas[:, t] % (2 * np.pi)
+    psi = np.sort(psi)
+
+    diff = np.diff(psi)
+    idcs = np.where(diff > dist / 2)[0]
+
+    clusters = []
+    n_changes = len(idcs)
+    for i in range(n_changes + 1):
+        start = idcs[i - 1] + 1 if i > 0 else None
+        end = idcs[i] + 1 if i < n_changes else None
+        clusters.append(psi[start:end])
+
+    is_k_clusters = True  # changed below if False
+
+    for i in range(n_changes + 1):
+        if np.mean(np.diff(clusters[i])) > atol:  # cluster is not compact
+            is_k_clusters = False
+
+    for i in range(n_changes):
+        dist_ij = abs(np.mean(clusters[i]) - np.mean(clusters[i + 1]))
+        if abs(dist_ij - dist) > atol:
+            is_k_clusters = False  # clusters have wrong distance between them
+
+    if n_clust == len(clusters):
+        sizes = [len(cluster) / N for cluster in clusters]
+    elif n_clust == clusters - 1:
+        sizes = [len(cluster) / N for cluster in clusters[:-1]]
+        sizes[0] += len(clusters[-1])  # 0th and last clusters are the same
+    else:
+        raise ValueError("k must be equal to or one unit below len(cluster)")
+
+    return is_k_clusters, sizes
 
 
 def identify_winding_number(thetas, t, atol=1e-1):
